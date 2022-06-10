67f76b43b6617cf1b41b9766a14cb949593fd956
tejas <achte.2022@gmail.com>
2022-06-09 20:55:52 -0700

    Update

---
+++ commands.sh
@@ -0,0 +1,15 @@
+#Count how many lines are there in “data.dat”.
+echo "Number of lines in data.dat: "
+wc -l data.dat
+
+# Count how many lines of those contain “dolor” or “dalor”?
+echo "Number of lines that contain dolor or dalor in data.dat: "
+egrep -w 'dolor|dalor' data.dat | wc -l
+
+# Count how many words are there in “data.dat”?
+echo "Number of words in data.dat: "
+wc -w data.dat
+
+# Count how many of those start with “mol”?
+echo "Number of words that begin with \"mol\" in data.dat: "
+grep -c '\bmol' data.dat
